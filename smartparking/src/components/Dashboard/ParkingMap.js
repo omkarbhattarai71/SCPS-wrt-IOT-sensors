12f824da7773bbdb63c974c7afb27aa4b40cbaeb
Sven Suneson <suneson96@gmail.com>
2025-12-01 10:28:09 +0100

    SCP-39 Auto focus map

--- smartparking/src/components/Dashboard/ParkingMap.js
+++ smartparking/src/components/Dashboard/ParkingMap.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import L from "leaflet";
 import { DEFAULT_MAP_CENTER, DEFAULT_MAP_ZOOM } from "../../constants/parkingConstants";
 import "leaflet/dist/leaflet.css";
@@ -12,6 +12,22 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
 
+// Component to fit map bounds to markers
+const FitBounds = ({ parkingLots }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (parkingLots && parkingLots.length > 0) {
+      const bounds = L.latLngBounds(
+        parkingLots.map(lot => [lot.location.latitude, lot.location.longitude])
+      );
+      map.fitBounds(bounds, { padding: [50, 50] });
+    }
+  }, [parkingLots, map]);
+
+  return null;
+};
+
 const ParkingMap = ({ token, parkingLots }) => {
   return (
     <MapContainer
@@ -20,6 +36,7 @@ const ParkingMap = ({ token, parkingLots }) => {
       style={{ height: "500px", marginTop: "20px" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <FitBounds parkingLots={parkingLots} />
       {token &&
         parkingLots.map((lot) => (
           <Marker
