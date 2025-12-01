8d85a0b42da043da00ec242b00ff8eba73f7c445
Sven Suneson <suneson96@gmail.com>
2025-12-01 10:10:47 +0100

    SCP-39 Remove unused constants

--- smartparking/src/constants/parkingConstants.js
+++ smartparking/src/constants/parkingConstants.js
@@ -1,15 +1,6 @@
 
 export const DEFAULT_MAP_CENTER = [55.65326433931066, 12.569958569333638];
 export const DEFAULT_MAP_ZOOM = 14;
-
-export const PARKING_STATUS = {
-  ALL: "all",
-  AVAILABLE: "available",
-  OCCUPIED: "occupied",
-};
-
-// Colors now use CSS variables defined in index.css
-// These can be used in JS where CSS variables aren't accessible
 export const COLORS = {
   primary: "var(--color-primary)",
   danger: "var(--color-danger)",
